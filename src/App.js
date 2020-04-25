7a2fc07448c57c53c65056e03f2103faae56bc3d
mordegar_admin <mordegar@inbox.ru>
2020-04-25 15:46:00 +0400

    Add fix routs components in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/header';
-import Nav from './components/nav';
-import Profile from './components/profile';
+import Header from './components/header/header';
+import Nav from './components/nav/nav';
+import Profile from './components/profile/profile';
 
 import './App.css';
 
