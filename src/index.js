10cacd0a6292c66190411fef8d6c9885705fbb07
mordegar_admin <mordegar@inbox.ru>
2020-05-01 06:16:19 +0400

    Store,getState,dispatch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { addPost, updateNewPostText, subscribe } from './redux/state';
 import { BrowserRouter } from "react-router-dom";
 
 
@@ -12,16 +11,17 @@ import { BrowserRouter } from "react-router-dom";
 let rerenderEntireTree = (state) => {
 	ReactDOM.render(
 	  <BrowserRouter>
-	    <App state = {state} addPost = {addPost} updateNewPostText = {updateNewPostText} />
+	    <App state = {state} 
+	    	dispatch = {store.dispatch.bind(store)} />
 	  </BrowserRouter>,
 	  document.getElementById('root')
 	);
 }
 
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,52 +1,65 @@
-let rerenderEntireTree = () => {
-	console.log("State was Changed");
-}
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const state = {
-	profilePage: {
-		posts: [
-			{id : 1, message : "Hi, how are you?", likesCount : 12},
-			{id : 2, message : "Lala Lala!))", likesCount : 34},
-			{id : 1, message : "SUKA BLEAT", likesCount : 5},
-			{id : 2, message : "It's my first post", likesCount : 51}
-		],
-		newPostText : "HA-ha-HA"
+let store = {
+	_state : {
+		profilePage: {
+			posts: [
+				{id : 1, message : "Hi, how are you?", likesCount : 12},
+				{id : 2, message : "Lala Lala!))", likesCount : 34},
+				{id : 1, message : "SUKA BLEAT", likesCount : 5},
+				{id : 2, message : "It's my first post", likesCount : 51}
+			],
+			newPostText : "HA-ha-HA"
+		},
+		dialogsPage: {
+			dialogs: [
+				{id : 1, name : 'Dima'},
+				{id : 2, name : 'Cveta'},
+				{id : 3, name : 'Sergey'},
+				{id : 4, name : 'Valera'},
+				{id : 5, name : 'Evgeniy'}
+			],
+			messages: [
+				{id : 1, message : 'Hi!'},
+				{id : 2, message : 'How is your it-kamasutra!?'},
+				{id : 3, message : 'Yo!Yo!Yo!'}
+			]
+		}
 	},
-	dialogsPage: {
-		dialogs: [
-			{id : 1, name : 'Dima'},
-			{id : 2, name : 'Cveta'},
-			{id : 3, name : 'Sergey'},
-			{id : 4, name : 'Valera'},
-			{id : 5, name : 'Evgeniy'}
-		],
-		messages: [
-			{id : 1, message : 'Hi!'},
-			{id : 2, message : 'How is your it-kamasutra!?'},
-			{id : 3, message : 'Yo!Yo!Yo!'}
-		]
+	_callSubscriber() {
+		console.log("State was Changed");
+	},
+
+	getState() {
+		return this._state;
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer;
+	},
+
+	dispatch(action) { // { type : 'ADD-POST'}
+		if (action.type === ADD_POST) {
+			let newPost = {
+				id : 5,
+				message : this._state.profilePage.newPostText,
+				likesCount : 0
+			};
+
+			this._state.profilePage.posts.push(newPost);
+			this._state.profilePage.newPostText = "";
+			this._callSubscriber(this._state);
+		} else if (action.type === UPDATE_NEW_POST_TEXT) {
+			this._state.profilePage.newPostText = action.newText;
+			this._callSubscriber(this._state);
+		}
 	}
-};
-
-export const addPost = () => {
-	let newPost = {
-		id : 5,
-		message : state.profilePage.newPostText,
-		likesCount : 0
-	};
-
-	state.profilePage.posts.push(newPost);
-	state.profilePage.newPostText = "";
-	rerenderEntireTree(state);
 }
 
-export const updateNewPostText = (newText) => {
-	state.profilePage.newPostText = newText;
-	rerenderEntireTree(state);
-}
+export const addPostActionCreator = () => 
+			({type : ADD_POST})
+export const updateNewPostTextActionCreator = (text) => 
+			({type : UPDATE_NEW_POST_TEXT, newText : text})
 
-export const subscribe = (observer) => {
-	rerenderEntireTree = observer;
-} 
-
-export default state;
+export default store;
+window.store = store;
